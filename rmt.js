a26d9bc0415d052416ed9792e9c7f8cd51d8dc91
Vin Samuel <vinscool@live.ca>
2021-10-05 00:45:02 -0400

    H- to B-
    It always bothered me in the original rmt.exe, no offence intended.

--- rmt.js
+++ rmt.js
@@ -177,7 +177,7 @@ const VOLUME_TAB = [
 	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
 ]
 
-const noteBaseNames = ['C-', 'C#', 'D-', 'D#', 'E-', 'F-', 'F#', 'G-', 'G#', 'A-', 'A#', 'H-'];
+const noteBaseNames = ['C-', 'C#', 'D-', 'D#', 'E-', 'F-', 'F#', 'G-', 'G#', 'A-', 'A#', 'B-'];
 const noteNames = _.times(61,note => `${noteBaseNames[note % 12]}${Math.floor(note/12)+1}`)
 
 
